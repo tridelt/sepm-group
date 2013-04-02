befcd2a49e32e8fae1832bd0823c4e79897b4c93
Julian Schrittwieser <J.Schrittwieser@gmail.com>
2013-04-03 00:20:46 +0200

    finer grained dash updates

--- update_dash.sh
+++ update_dash.sh
@@ -1,4 +1,11 @@
 cd /home/mononofu/sepm-group/dashboard
-dashing job commits EeLg5ZnXHTMeygHF7DVxptN9A56UxsXu4pN
-dashing job tests EeLg5ZnXHTMeygHF7DVxptN9A56UxsXu4pN
-dashing job todos EeLg5ZnXHTMeygHF7DVxptN9A56UxsXu4pN
+case "$1" in
+  "start")
+    dashing job commits EeLg5ZnXHTMeygHF7DVxptN9A56UxsXu4pN
+    dashing job todos EeLg5ZnXHTMeygHF7DVxptN9A56UxsXu4pN
+    ;;
+  "end")
+    dashing job tests EeLg5ZnXHTMeygHF7DVxptN9A56UxsXu4pN
+    ;;
+esac
+
