1981c0009c294d28a830d5e804a73ce30d66c399
Julian Schrittwieser <J.Schrittwieser@gmail.com>
2013-04-18 00:21:08 +0200

    perform cppcheck on build

--- run_tests.sh
+++ run_tests.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+MACHINE_TYPE=`uname -m`
+
 mkdir -p build
 set -e
 cd build
@@ -6,3 +8,11 @@ rm -f tests/reports/*.xml
 cmake ..
 make test
 make doc
+
+if [ ${MACHINE_TYPE} == 'x86_64' ]; then
+  CPPCHECK="../cmake/cppcheck_amd64"
+else
+  CPPCHECK="../cmake/cppcheck_x86"
+fi
+
+$CPPCHECK .. -i ../build -i ../dependencies --std=c++11 --enable=all --xml-version=2 2> style_issues.xml
