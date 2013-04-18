958f524627b0638c9a6655dd16ed1a5388d2fc23
Julian Schrittwieser <j.schrittwieser@gmail.com>
2013-04-18 11:56:00 +0200

    one more test

--- tests/client/basic_tests.cpp
+++ tests/client/basic_tests.cpp
@@ -4,6 +4,10 @@ TEST(SanityTest, CanTest) {
   EXPECT_EQ(1, 1);
 }
 
+TEST(SanityTest, CanTestMore) {
+  EXPECT_EQ(2, 2);
+}
+
 TEST(SanityTest, CStringEquality) {
   EXPECT_STREQ("hello", "hello");
 }
