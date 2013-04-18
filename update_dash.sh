f05be57543e8a5a0b09e7181bff427bc33902241
Julian Schrittwieser <J.Schrittwieser@gmail.com>
2013-04-18 15:39:59 +0200

    and check build status on start

--- update_dash.sh
+++ update_dash.sh
@@ -1,6 +1,7 @@
 cd /home/mononofu/sepm-group/dashboard
 case "$1" in
   "start")
+    dashing job build_status EeLg5ZnXHTMeygHF7DVxptN9A56UxsXu4pN
     dashing job commits EeLg5ZnXHTMeygHF7DVxptN9A56UxsXu4pN
     dashing job todos EeLg5ZnXHTMeygHF7DVxptN9A56UxsXu4pN
     dashing job cppcheck EeLg5ZnXHTMeygHF7DVxptN9A56UxsXu4pN
