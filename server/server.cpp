d945ebfe4fe3edfa1b5903caa3a42b50faaccab6
Julian Schrittwieser <J.Schrittwieser@gmail.com>
2013-04-28 23:30:02 +0200

    removed zmq

--- server/server.cpp
+++ server/server.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <boost/program_options.hpp>
 #include "ExitHandler.h"
-#include "zmqpp/zmqpp.hpp"
 #include "SecureDistributedChat.h"
 
 namespace po = boost::program_options;
@@ -24,14 +23,6 @@ int main(int argc, char** argv) {
 
   cout << "Hello from server" << endl;
 
-  zmqpp::context context;
-
-  zmqpp::socket message_source(context, zmqpp::socket_type::pub);
-  message_source.bind("tcp://*:5555");
-
-  zmqpp::socket message_sink(context, zmqpp::socket_type::sub);
-  message_sink.bind ("tcp://*:5556");
-  message_sink.subscribe("");
 
   ExitHandler::i()->setHandler([](int) {
     // called when SIGINT (eg by Ctrl+C) is received
@@ -41,23 +32,6 @@ int main(int argc, char** argv) {
     // this is just to show the handler is working
     cout << " Got signal .. terminating" << endl;
   });
-
-  try {
-    while (true) {
-      string msg;
-
-      // wait for message
-      message_sink.receive(msg);
-      cout << "Received msg: [" << msg << "]" << endl;
-
-      // Send reply back to client
-      message_source.send(msg);
-    }
-  } catch (zmqpp::zmq_internal_exception) {
-    cout << "Stopping zeromq.." << endl;
-    message_source.close();
-    message_sink.close();
-  }
 
   return 0;
 }
