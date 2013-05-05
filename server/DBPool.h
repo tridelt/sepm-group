ecee7eeecdef1aa3749f62153c0424f32f609082
Julian Schrittwieser <j.schrittwieser@gmail.com>
2013-05-05 15:11:31 +0200

    document database access

--- server/DBPool.h
+++ server/DBPool.h
@@ -6,8 +6,28 @@
 
 class DBPool {
 public:
+  /**
+   *  singleton accessor for the connection pool manager
+   *
+   * the global connection pool manager is responsible for details such
+   * as which database to use (SQLite for development, PostgreSQL on the
+   * production server), how many concurrent connections to use, etc.
+   * @return an instance of the global connection pool manager
+   */
   static DBPool* i();
 
+  /**
+   * get global connection pool to construct thread local connection
+   *
+   * SOCI is not threadsafe per-se, but thread safe connections can be achieved
+   * by constructing them on demand. To save on overhead, a global connection
+   * pool is provided.
+   *
+   * actually getting a session is as simple as: `soci::session sql(DBPool::i()->getPool());`
+   *
+   * for details see: http://soci.sourceforge.net/doc/3.2/multithreading.html
+   * @return the global connection pool
+   */
   soci::connection_pool& getPool();
 private:
   static DBPool* instance;
