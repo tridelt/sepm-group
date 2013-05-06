4c1af368ff369887725b0bd7dbbad56e82b01654
Julian Schrittwieser <J.Schrittwieser@gmail.com>
2013-05-06 15:28:30 +0200

    be more liberal with input

--- server/AuthenticationImpl.cpp
+++ server/AuthenticationImpl.cpp
@@ -11,14 +11,15 @@ using namespace soci;
 
 void AuthenticationImpl::registerUser(const sdc::User &u, const string &pw,
   const Ice::Current&) {
-  if(!sdc::sdcHelper::isValidID(u.ID))
-    throw sdc::AuthenticationException("invalid ID");
+  // Be conservative in what you send, be liberal in what you accept
+  string name = u.ID;
+  if(sdc::sdcHelper::isValidID(u.ID))
+     name = sdc::sdcHelper::getNameFromID(u.ID);
 
   session sql(DBPool::i()->getPool());
 
   sql << "CREATE TABLE IF NOT EXISTS users(id text, pw text, pubkey text)";
 
-  auto name = sdc::sdcHelper::getNameFromID(u.ID);
   boost::optional<string> id;
   sql << "SELECT id FROM users WHERE id = :id",
     into(id), use(name);
@@ -37,14 +38,15 @@ void AuthenticationImpl::registerUser(const sdc::User &u, const string &pw,
 
 sdc::SessionIPrx AuthenticationImpl::login(const sdc::User &u, const string &pw,
     const Ice::Identity&, const Ice::Current&) {
-  if(!sdc::sdcHelper::isValidID(u.ID))
-    throw sdc::AuthenticationException("invalid ID");
+  // Be conservative in what you send, be liberal in what you accept
+  string name = u.ID;
+  if(sdc::sdcHelper::isValidID(u.ID))
+     name = sdc::sdcHelper::getNameFromID(u.ID);
 
   session sql(DBPool::i()->getPool());
 
   sql << "CREATE TABLE IF NOT EXISTS users(id text, pw text, pubkey text)";
 
-  auto name = sdc::sdcHelper::getNameFromID(u.ID);
   boost::optional<string> id, storedPw, pubkey;
   sql << "SELECT id, pw, pubkey FROM users WHERE id = :id",
     into(id), into(storedPw), into(pubkey), use(name);
@@ -60,15 +62,16 @@ sdc::SessionIPrx AuthenticationImpl::login(const sdc::User &u, const string &pw,
   if(providedPubkey != "" && providedPubkey != pubkey.get())
     throw sdc::AuthenticationException("public key can't change");
 
-  // TODO: check callback (call echo) provided by identity before logging in
+  // TODO: check callback provided by identity before logging in (call echo)
 
   // don't log passwords!
   INFO("Logging in ", u.ID);
   sdc::User user;
-  user.ID = u.ID;
+  user.ID = name;
 
-  // make sure the pubkey from registering is used
+  // make sure the pubkey from registering is used, not some other key
   user.publicKey = sdc::ByteSeq(pubkey.get().begin(), pubkey.get().end());
+
   // TODO: make sure session created here doesn't leak
   auto proxy = server->exposeObject(new SessionImpl(user));
   return sdc::SessionIPrx::checkedCast(proxy);
