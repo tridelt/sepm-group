be876091d20c5ac21caf4741b8eab556303fc097
Julian Schrittwieser <J.Schrittwieser@gmail.com>
2013-05-06 21:15:36 +0200

    add db config so I can delete the email

---
+++ common/config.h
@@ -0,0 +1,11 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#include <string>
+
+using namespace std;
+
+const string DB_USER = "grp_4";
+const string DB_PASS = "18fef7c7-f606-4d19-87a8-d6697d28efd7";
+
+#endif
