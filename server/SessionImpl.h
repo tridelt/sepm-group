98b36586557fbb1bc929edfc5b7d3701883def3f
v5r <github@b8000h.org>
2013-05-12 16:16:24 +0200

    deleteUser

--- server/SessionImpl.h
+++ server/SessionImpl.h
@@ -16,11 +16,11 @@ public:
   void invite(const sdc::User &other, const string &chat, const sdc::ByteSeq &key, const Ice::Current&);
   void sendMessage(const sdc::ByteSeq &msg, const string &chat, const Ice::Current&);
   void deleteUser(const sdc::User &user, const Ice::Current&);
-  void saveLog(const string&, Ice::Long, const sdc::SecureContainer&, const Ice::Current&) {}
-  sdc::Loglist retrieveLoglist(const Ice::Current&) { return sdc::Loglist(); }
-  sdc::SecureContainer retrieveLog(const string&, Ice::Long, const Ice::Current&) { return sdc::SecureContainer();}
-  void saveContactList(const sdc::SecureContainer&, const Ice::Current&) {}
-  sdc::SecureContainer retrieveContactList(const Ice::Current&) { return sdc::SecureContainer(); }
+  void saveLog(const string&, Ice::Long, const sdc::SecureContainer&, const Ice::Current&);
+  sdc::Loglist retrieveLoglist(const Ice::Current&);
+  sdc::SecureContainer retrieveLog(const string&, Ice::Long, const Ice::Current&);
+  void saveContactList(const sdc::SecureContainer&, const Ice::Current&);
+  sdc::SecureContainer retrieveContactList(const Ice::Current&);
 private:
   bool loggedIn;
   sdc::User user;
